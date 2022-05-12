106cb47a91d454586d3ad9294ca30c7dc96e6891
Navneet <97830768+navneetnair7@users.noreply.github.com>
2022-05-12 16:02:36 +0530

    Create default_values.java

---
+++ default_values.java
@@ -0,0 +1,16 @@
+class test{
+    static int a;
+    static float b;
+    static double c;
+    static char d;
+    static String e;
+    static boolean f;
+    public static void main(String[] args){
+        System.out.println("Default value of int is: " + a);
+        System.out.println("Default value of float is: " + b);
+        System.out.println("Default value of double is: " + c);
+        System.out.println("Default value of char is: " + d);
+        System.out.println("Default value of String is: " + e);
+        System.out.println("Default value of boolean is: " + f);
+    }
+}
